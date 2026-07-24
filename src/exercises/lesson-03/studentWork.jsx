8832f4a897dc405dbf60e059b8c4735e1d5df85d
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-07-24 11:21:26 +0530

    Fixing bugs

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -1,12 +1,20 @@
 //Lesson-03 Component Lifecycle, Hooks, State, and Props
 //Exercise: React Bug Hunt – Fix the broken components in this folder
-//Impport components here
+//Import components here
+
+import BugEffectLoop from './exercises/lesson-03/BugEffectLoop.jsx';
+import BugMutatedState from './exercises/lesson-03/BugMutatedState.jsx';
+import BugProps from './exercises/lesson-03/BugProps.jsx';
 
 export default function StudentWork() {
   return (
     <div>
       {/* add components here */}
-      <p>Student output will go here</p>
+      {/* <p>Student output will go here</p> */}
+
+      <BugEffectLoop />
+      <BugMutatedState />
+      <BugProps />
     </div>
   );
 }

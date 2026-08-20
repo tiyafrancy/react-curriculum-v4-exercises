21ef710f743da2cf4999a7d613dfa90b0ad582a7
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-08-19 22:37:35 -0500

    changes in FetchOnRender.jsx file

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,24 @@
 import './Lesson07Styles.css';
+import { useEffect, useState } from 'react';
+import { getPosts } from './api';
 
 export default function FetchOnRender() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts().then((data) => setPosts(data));
+  }, []);
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
